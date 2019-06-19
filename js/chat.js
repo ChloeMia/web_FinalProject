92229f0f9b67d818f1ff51c18779916c2d37f6db
icelala <47877399+icelala@users.noreply.github.com>
2019-06-20 03:08:54 +0800

    Add files via upload

---
+++ js/chat.js
@@ -0,0 +1,17 @@
+function chat(e) {
+    if(e.keyCode == 13){
+        db.collection('message').doc().set({
+            name : getCookie("name"),
+            message : document.getElementById("chat").value
+        })
+        return false;
+    }
+}
+
+function initTextArea(){
+    var signin = getCookie("signin");
+    if(signin == ""){
+        document.getElementById("chat").innerHTML = "Please sign in first"
+        document.getElementById("chat").disabled = true;
+    }
+}
--- js/database.js
+++ js/database.js
@@ -1,25 +1,30 @@
 function login() {
-    db.collection('test').get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            if(doc.data().account == document.forms["userform"]["account"].value &&
-                doc.data().password == document.forms["userform"]["password"].value){
-                    window.location.href ="index.html"
+    db.collection('test').doc(document.forms["userform"]["account"].value).get().then((doc) => {
+        if(doc.exists){
+            if(doc.data().password == document.forms["userform"]["password"].value){
+                //window.location.href ="index.html"
+                document.cookie = "name=" + doc.data().name + ";" + "signin=true";
             }
-        })
+            else{
+                alert("Wrong password");
+            }
+        }
+        else{
+            alert("Couldn't find this account");
+        }
     });
     return false;
 }
 
 function createAccount() {
-    console.log(document.forms["userform"]["name"].value);
-    console.log(document.forms["userform"]["account"].value);
-    console.log(document.forms["userform"]["password"].value);
     db.collection('test').doc(document.forms["userform"]["name"].value).set({
         name : document.forms["userform"]["name"].value,
         account : document.forms["userform"]["account"].value,
-        password : document.forms["userform"]["password"].value
+        password : document.forms["userform"]["password"].value,
     }).then(function(){
-        window.location.href ="index.html"
-    })
+        window.location.href ="index.html";
+        document.cookie = "name=" + doc.data().name + ";" + "signin=true";
+    });
     return false;
 }
+
